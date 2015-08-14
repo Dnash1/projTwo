ad917264d92b9055fbc442873cb79d78d1be381b
David Nash <david.allyxanddar.nash@gmail.com>
2015-08-14 14:37:54 -0400

    cascading suicide system

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ var db = new sqlite3.Database('forum.db');
 var fs = require('fs');
 var ejs = require('ejs');
 
+
 var bodyParser = require('body-parser');
 var urlencodedBodyParser = bodyParser.urlencoded({extended: false});
 app.use(urlencodedBodyParser);
 var methodOverride = require('method-override');
 app.use(methodOverride('_method'));
+app.use(express.static('public'));
 
 app.get("/", function(req, res) {
 	db.all("SELECT * FROM boards", function(err, rows) {
@@ -38,17 +40,25 @@ app.get("/catalog/:id", function(req, res) {
 });
 
 app.get("/thread/:id", function(req, res) {
-	db.all("SELECT * FROM posts WHERE thread_id = ?",
+	db.get("SELECT * FROM threads WHERE id=?",
 		req.params.id,
-		function(err, rows) {
+		function(err, rods) {
 			if (err) {
 				console.log(err);
 			} else {
-				var html = fs.readFileSync("./thread.html", "utf8");
-				var rend = ejs.render(html, {posts: rows});
-				res.send(rend);
+					db.all("SELECT * FROM posts WHERE thread_id=?",
+					req.params.id,
+					function(err, rows) {
+					if (err) {
+						console.log(err);
+					} else {
+						var html = fs.readFileSync("./thread.html", "utf8");
+						var rend = ejs.render(html, {thread: rods, posts: rows});
+						res.send(rend);
+					}
+				});
 			}
-		});
+		});	
 });
 
 app.listen(3000, function() {
--- schema.sql
+++ schema.sql
@@ -17,8 +17,8 @@ CREATE TABLE posts (
 	id integer PRIMARY KEY AUTOINCREMENT,
 	thread_id integer,
 	user_id integer,
-	subject varchar,
-	body varchar,
+	subj varchar,
+	bod varchar,
 	image varchar,
 	replied_to integer
 );
@@ -36,5 +36,7 @@ INSERT INTO boards (name, image) VALUES ('music', 'ph');
 INSERT INTO boards (name, image) VALUES ('television', 'ph');
 INSERT INTO boards (name, image) VALUES ('technology', 'ph');
 
-INSERT INTO threads (board_id, user_id, subject, body, image) VALUES (1, 4, 'Shitposts', 'Shitpooooost', './forums.jpg');
-INSERT INTO threads (board_id, user_id, subject, body, image) VALUES (1, 4, 'Bloo', 'Shmee', 'http://i.imgur.com/g19vTEt.jpg');
+INSERT INTO threads (board_id, user_id, subject, body, image) VALUES (1, 4, 'Shitposts', 'Shitpooooost', 'x');
+INSERT INTO threads (board_id, user_id, subject, body, image) VALUES (1, 4, 'Bloo', 'Shmee', 'x');
+
+INSERT INTO posts (thread_id, user_id, subj, bod, image, replied_to) VALUES (1, 2, 'nlah', 'haha fuck you!', '/', 3);
